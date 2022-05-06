819ea7f6b6970d945efeddf7b2d5089944c22519
Philipp Weinmann <philipp.weinmann71@gmail.com>
2022-05-06 12:39:04 +0200

    only logging in once now

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,22 @@
 from locust import HttpUser, task
+from locust import events
+
 
 
 class HelloWorldUser(HttpUser):
-    @task
-    def hello_world(self):
+    def on_start(self):
         login_data = {
             "email": "stefan+1@hivebuy.de",
             "password": ""
         }
 
-        response = self.client.get(url="/api/auth/", data=login_data)
+        response = self.client.get(url="api/auth/", data=login_data)
+        self.cookie_jar = response.cookies
 
+    @task
+    def hello_world(self):
+        user_url = "api/company/user/"
+        self.client.get(url=user_url, cookies=self.cookie_jar)
 
-if __name__ == '__main__':
-    HelloWorldUser().run()
+        pur_url = "api/company/purchase-request/"
+        self.client.get(url=pur_url, cookies=self.cookie_jar)
