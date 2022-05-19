4d9c96ce591a555ccca9db92695662a7b43a0b28
Philipp Weinmann <philipp.weinmann71@gmail.com>
2022-05-19 11:34:08 +0200

    ...

--- common/auth.py
+++ common/auth.py
@@ -5,12 +5,9 @@ from urls import HivebuyUrls
 
 def login(client):
     login_data = {
-        "email": os.environ.get("STEFAN_EMAIL", "stefan+1@hivebuy.de"),
-        "password": os.environ.get("STEFAN_PASSWORD", "Change-Me1!")
+        "email": os.environ.get("STEFAN_EMAIL", ""),
+        "password": os.environ.get("STEFAN_PASSWORD", "")
     }
 
     # cookies are automatically saved and sent
-    response = client.get(url=HivebuyUrls.AUTH_URL.value, data=login_data)
-
-    # don't continue if the authentification failed
-    # response.raise_for_status()
+    client.get(url=HivebuyUrls.AUTH_URL.value, data=login_data)
